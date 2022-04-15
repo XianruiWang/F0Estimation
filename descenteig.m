45aac22b093c0899d3d0385b27fa49a5d59faf90
Wang Xianrui <13022913172@126.com>
2022-04-15 15:27:36 +0800

    Update descenteig.m

--- descenteig.m
+++ descenteig.m
@@ -8,31 +8,5 @@ function [V, D] = descenteig(R)
 %     D : Eigenvalue matrix
 %     V: eigenvector matrix
 %
-% ------
-% Author:  Jigndong Chen, Ph.D.
-% Copyright 2012(c)
-%
-if (nargin ~= 1)
-    error('Incorrect number of inputs!');
-end
-L = size(R, 1);
-[HV, HD] = eig(R);
-for i = 1 : L
-    for j = i: L
-        if HD(i,i) < HD(j,j)
-            tempD = HD(i,i);
-            HD(i,i) = HD(j,j);
-            HD(j,j) = tempD;
-            tempV = HV(:,i);
-            HV(:,i) = HV(:,j);
-            HV(:,j) = tempV;
-        end
-    end
-end
-if nargout == 1
-    D = HD;
-end
-if nargout == 2
-    D = HD;
-    V = HV;
-end
+%%
+% please rewrite this function by yourself or directly use matlab EVD
